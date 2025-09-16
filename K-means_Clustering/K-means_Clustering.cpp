7594d19319dd81bf68fcf0a3f60f962089b7b7ac
Christopher Sefcik <cdsefcik@gmail.com>
2025-09-16 17:16:51 -0400

    Add files via upload
    Initial Commit.

---
+++ K-means_Clustering/K-means_Clustering.cpp
@@ -0,0 +1,27 @@
+﻿/*
+K - means_Clustering.cpp : Defines the entry point for the application.
+
+Author: Christopher Sefcik
+
+Program Use: Optimized version of K-means Clustering
+
+Development Notes:
+
+How to run program:
+
+*/ 
+
+#include "K-means_Clustering.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+
+//Input File Code:
+
+
+
+//Output File Code:
+	return 0;
+}
